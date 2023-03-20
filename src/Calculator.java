f16a091ff8d521762b22ddcfb74df3de8f673360
Ymivai <danylo010204@gmail.com>
2023-03-20 21:32:19 +0200

    Uppdate

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,56 +3,46 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
-        int operand1= 128, operand2= 0;
-
+        System.out.println("Enter two numbers for operation");
         Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt(), number2 = scanner.nextInt();
+        boolean b = true;
 
-        System.out.println("\n"+operand1+"__"+operand2);
-        System.out.print("\nВведіть один із зазначених операторів (+; -; *; /; %): ");
 
-        String sign= scanner.next();
-
-        boolean b =true;
-        //
         //       Програма не відпрацює поки користувач не введе один із зазначених операторів, який буде знов
 //        перевірений перемикачем switch-case на наступній ітерації циклу while. У випадку, якщо він
 //        співпаде з одним із постійних значень оператора case, умова циклу while зміниться на false і
 //        робота циклу припиниться. У випадку ж якщо користувач не ввів жодний із зазначених операторів,
 //        робота циклу піде на новий виток. И не только
 
-        while (b) {
 
-            switch (sign) {
-                case ("+"):
-                    System.out.println("\n"+operand1 + " + " + operand2 + "= " + (operand1 + operand2));
-                    b=false;
+        while (b) {
+            System.out.print("\nEnter one of them (+; -; *; /; %)");
+            String opeand = scanner.next();
+            switch (opeand) {
+                case "+":
+                    System.out.println("\n" + number1 + " + " + number2 + "= " + (number1 + number2));
                     break;
-                case ("-"):
-                    System.out.println("\n"+operand1 + " - " + operand2 + "= " + (operand1 - operand2));
-                    b=false;
+                case "-":
+                    System.out.println("\n" + number1 + " - " + number2 + "= " + (number1 - number2));
                     break;
-                case ("*"):
-                    System.out.println("\n"+operand1 + " * " + operand2 + "= " + (operand1 * operand2));
-                    b=false;
+                case "*":
+                    System.out.println("\n" + number1 + " * " + number2 + "= " + (number1 * number2));
                     break;
-                case ("/"):
-                    if(operand2 ==0){
-                        System.out.println("\nПомилка! \nСпроба ділення на 0");
+                case "/":
+                    if (number2 == 0) {
+                        System.out.println("\nError");
                     }
-                    b=false;
+                    System.out.println("\n" + number1 + " / " + number2 + "= " + (float)(number1 / number2));
                     break;
-                case ("%"):
-                    if(operand2 ==0){
-                        System.out.println("\nПомилка! \nСпроба ділення на 0");
-                    }
-                    b=false;
+                case "leave":
+                    b = false;
                     break;
                 default:
-                    System.out.print("\nВаш оператор не відповідає жодному вищезазначеному," +
-                            "\nповторіть,будь ласка, спробу: ");
-                    sign = scanner.next();
+                    System.out.print("\nOperation isn't initial. Try more");
                     break;
             }
         }
+        System.out.println("Good night");
     }
 }
